beaf992ddd87fe3ae735a439d213904cc51a0b72
jdenoc <jdenoc@gmail.com>
2013-10-05 14:41:06 -0500

    * Added ENT_QUOTES, so HTML is decoded properly with quotation marks. * For mobile version, when an article is loaded, the recorded stamp is now visible at the bottom of the article.

--- php_scripts/load_article.php
+++ php_scripts/load_article.php
@@ -22,13 +22,14 @@ if(isset($_REQUEST['m'])){
     $text  = "<script type='text/javascript'>var viewed=".$article['viewed'].", marked=".$article['marked'].";</script>\r\n";
     $text .= "<li class=\"list_item article\">\r\n";
     $text .= '  <h3><a href="'.$article['link'].'" target="_blank">'.html_entity_decode($article['title']).'</a></h3>'."\r\n";
-    $text .= '  '.html_entity_decode($article['content'])."\r\n";
+    $text .= '  '.html_entity_decode($article['content'], ENT_QUOTES)."\r\n";
     $text .= "  <div class=\"article_sub\">\r\n";
     $text .= '      <label style="display: inline; margin-right: 20px"><input type="checkbox" style="margin: -3px 0 0 8px;" onclick="markRead('.$article_id.', viewed)" '.$read.'/> Marked Read</label>';
     $text .= '      <label style="display: inline;"><span class="badge badge-warning"  style="margin-right:0">';
     $text .= '          <input type="checkbox" style="margin: 0 -3px 0 -2px;" onclick="markArticle('.$article_id.', marked)" '.$marked.'/>';
     $text .= '      </span> Mark for Later</label>'."\r\n";
     $text .= '      <div style="text-align: center; margin-top: 15px"><a onclick="$(\'body\').scrollTo(0)">Scroll to Top</a></div>';
+    $text .= '      <strong style="text-align:center; margin-top: 10px; display:block;font-size:10px">'.$article['stamp'].'</strong>';
     // TODO - add sharing features.
     $text .= "  </div>\r\n";
     $text .= "</li>\r\n";
@@ -36,7 +37,7 @@ if(isset($_REQUEST['m'])){
 } else {
     $text  = "<div class=\"article\">\r\n";
     $text .= '  <h3><a href="'.$article['link'].'" target="_blank">'.html_entity_decode($article['title']).'</a></h3>'."\r\n";
-    $text .= '  '.html_entity_decode($article['content'])."\r\n";
+    $text .= '  '.html_entity_decode($article['content'], ENT_QUOTES)."\r\n";
     $text .= "  <div class='article_sub'>\r\n";
     $text .= '      <label style="display: inline; margin-right: 20px"><input type="checkbox" style="margin: -3px 0 0 8px;" onclick="markRead('.$article_id.')" '.$read.'/> Marked Read</label>';
     $text .= '      <label style="display: inline;"><span class="badge badge-warning"  style="margin-right:0"><input type="checkbox" style="margin: 0 -3px 0 -2px;" onclick="markArticle('.$article_id.')" '.$marked.'/></span> Marked for Later</label>'."\r\n";
