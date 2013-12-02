26655a6508f7b0db647f820fba22f87f807fe5c3
jdenoc <jdenoc@gmail.com>
2013-12-01 18:02:46 -0600

    Fixed bug that caused the deletion of "Marked for Later" articles, instead of just unmarked and viewed articles.

--- cron/clear_read.php
+++ cron/clear_read.php
@@ -8,4 +8,4 @@
 require_once(dirname(__FILE__) . '/../includes/connection.php');
 $db = new pdo_connection('jdenocco_rss');
 
-$db->delete("feed_articles", 'viewed=1 AND stamp < :today', array('today'=>date('Y-m-d', strtotime('-1 month'))));
+$db->delete("feed_articles", 'viewed=1 AND stamp < :today AND marked=0', array('today'=>date('Y-m-d', strtotime('-1 month'))));
